85189cab457decdb667f15b3a5fa9f6448c06b0a
takacsberni <takacsberni@gmail.com>
2022-09-14 13:11:26 +0200

    feat: index.js returns now a TerminatorLister instead of only one Terminator

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
-import Terminator from "./components/Terminator";
 import {models1} from "./models";
+import TerminatorLister from "./components/temrinator-lister";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <Terminator models1={models1}/>
+    <TerminatorLister models1={models1}/>
 );
