752437ed139aec34bc22461f376feff4e8583f5d
takacsberni <takacsberni@gmail.com>
2022-09-14 13:13:55 +0200

    feat: add some tachyons style

--- src/components/Terminator.jsx
+++ src/components/Terminator.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Terminator = ( {id, name, serialNumber}) => {
     return (
-        <div>
+        <div className='bg-light-blue dib br3 pa3 ma2 grow tc'>
             <img src={`https://robohash.org/${id}?size=180x180`} alt = 'terminator' />
             <div>
                 <h2> {name} </h2>
