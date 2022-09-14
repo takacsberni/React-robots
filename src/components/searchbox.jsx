084837ce09e9182bd07975751c6084f068144bdf
takacsberni <takacsberni@gmail.com>
2022-09-14 14:45:56 +0200

    feat: add a new component to implement a search option

---
+++ src/components/searchbox.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SearchBox = () => {
+    return (
+        <div className="pa2">
+            <input type='search' placeholder='Find a model: ' className="bg-light-blue pa3 ba b--green " />
+
+        </div>
+    )
+}
+
+export default SearchBox;
