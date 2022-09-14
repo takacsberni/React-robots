c3833ec848d43a08755e5565faf5b0f3fd0da5c7
takacsberni <takacsberni@gmail.com>
2022-09-14 12:50:18 +0200

    feat: add models

---
+++ src/models.js
@@ -0,0 +1,52 @@
+export const models1 = [
+    {
+        id: 1,
+        name: "T-X",
+        serialNumber: 202711110001
+    },
+    {
+        id: 2,
+        name: "T-1000",
+        serialNumber: 202711110002
+    },
+    {
+        id: 3,
+        name: "T-800",
+        serialNumber: 202711110003
+    },
+    {
+        id: 4,
+        name: "T-3000",
+        serialNumber: 202711110004
+    },
+    {
+        id: 5,
+        name: "T-600",
+        serialNumber: 202711110005
+    },
+    {
+        id: 6,
+        name: "T-900",
+        serialNumber: 202711110006
+    },
+    {
+        id: 7,
+        name: "T-950",
+        serialNumber: 202711110007
+    },
+    {
+        id: 8,
+        name: "T-3000",
+        serialNumber: 202711110004
+    },
+    {
+        id: 9,
+        name: "T-X",
+        serialNumber: 202711110009
+    },
+    {
+        id: 10,
+        name: "HK-Drone",
+        serialNumber: 202711110010
+    },
+]
