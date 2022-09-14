5504d0c611bcd04eb21d80c3cd9fd09bf773adfb
takacsberni <takacsberni@gmail.com>
2022-09-14 20:26:55 +0200

    feat: define a state for the search field

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import {models1} from "./models";
 import SearchBox from "./components/searchbox";
 
 class App extends React.Component{
+    constructor() {
+        super();
+        this.state ={
+            models: models1,
+            searchfield: ''
+        }
+    }
+
     render() {
         return (
             <div className="tc">
