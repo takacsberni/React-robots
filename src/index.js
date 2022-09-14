cfe7a441e804a65b9a40528bb3992365f83be855
takacsberni <takacsberni@gmail.com>
2022-09-14 13:06:48 +0200

    feat: give models as props to Terminator

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'tachyons';
+import Terminator from "./components/Terminator";
+import {models1} from "./models";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <Terminator models1={models1}/>
 );
