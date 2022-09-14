4832caa417d79ac652a074212474421016440725
takacsberni <takacsberni@gmail.com>
2022-09-14 14:46:16 +0200

    feat: App returns a SearchBox component too

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TerminatorLister from "./components/temrinator-lister";
 import {models1} from "./models";
+import SearchBox from "./components/searchbox";
 
 const App = () => {
     return (
-        <div>
+        <div className="tc">
+            <SearchBox />
             <TerminatorLister models1={models1} />;
         </div>
     );
