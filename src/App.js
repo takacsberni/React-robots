130193eb511015cf76bbf70f424afe4935494313
takacsberni <takacsberni@gmail.com>
2022-09-14 20:15:55 +0200

    feat: transform App into a class component (practising)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import TerminatorLister from "./components/temrinator-lister";
 import {models1} from "./models";
 import SearchBox from "./components/searchbox";
 
-const App = () => {
-    return (
-        <div className="tc">
-            <SearchBox />
-            <TerminatorLister models1={models1} />;
-        </div>
-    );
+class App extends React.Component{
+    render() {
+        return (
+            <div className="tc">
+                <SearchBox />
+                <TerminatorLister models1={models1} />;
+            </div>
+        );
+    }
 }
 
 export default App
