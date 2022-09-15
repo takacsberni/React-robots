6707a39945a84ce5bc3d3e420b4f2d5b66381ddc
takacsberni <takacsberni@gmail.com>
2022-09-15 09:59:13 +0200

    feat: starting to into transform App.js into a function component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import TerminatorLister from "./components/temrinator-lister";
 import {models1} from "./models";
 import SearchBox from "./components/searchbox";
 
-class App extends React.Component{
-    constructor() {
-        super();
-        this.state ={
-            models: models1,
-            searchField: ''
-        }
-    }
-
-    onSearchChange = (event) => {
+function App ()  {
+    // constructor() {
+    //     super();
+    //     this.state ={
+    //         models: models1,
+    //         searchField: ''
+    //     }
+    // }
+    const onSearchChange = (event) => {
         this.setState( {searchField: event.target.value})
         // console.log(event.target.value);
     }
 
-    render() {
+    return (){
         const filteredModels = this.state.models.filter( (model) => {
             return model.name.toLowerCase().includes(this.state.searchField.toLowerCase())
         })
